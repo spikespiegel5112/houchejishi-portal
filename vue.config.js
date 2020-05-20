389deb9ee074826eb7fb113b006c8a1ca6467ddf
spikespiegel5112 <583288277@qq.com>
2020-05-20 17:29:32 +0800

    temp

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,11 @@
 // vue.config.js
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "./",
   chainWebpack: config => {
     config
       .plugin('html')
       .tap(args => {
-        args[0].title= '吉市分期'
+        args[0].title = '吉市分期'
         return args
       })
   }
